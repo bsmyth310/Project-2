760531fa91f0539401c62d413b9f9e5cd2facb87
hussrizk56 <117960857+hussrizk56@users.noreply.github.com>
2023-03-14 21:23:34 -0400

    added variable and export function and added user code

--- models/model.js
+++ models/model.js
@@ -4,5 +4,16 @@ const workout = {
     selectAll: function() {
         return orm.selectAll('workouts');
     },
-
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals) {
+        return orm.insertOne('workouts', cols, vals);
+    },
+    updateOne: function(objColVals, condition) {
+        return orm.updateOne('workouts', objColVals, condition);
+    },
+    deleteOne: function(condition) {
+        return orm.deleteOne('workouts', condition);
+    }
 };
+// export the workout model to use in the workouts_controller
+module.exports = workout;
