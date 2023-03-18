a0bd61ad5abdda3145177933026b066d588a32c3
hussrizk56 <117960857+hussrizk56@users.noreply.github.com>
2023-03-18 08:39:10 -0400

    deleted item since no longer part of project

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.use(session({
 app.get('/', (req, res) => { res.render("main") });
 app.get('/login', (req, res) => { res.render("login") });
 app.get('/signup', (req, res) => { res.render("signup") });
-app.get('/aboutUs', (req, res) => { res.render("aboutUs") });
 app.get('/dashboard', (req, res) => { res.render("dashboard") });
 
 app.use = (routes);
